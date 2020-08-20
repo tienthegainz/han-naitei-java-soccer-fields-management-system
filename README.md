3bc15345628ae8d7397030be13a3b3e9e5a7691e
Tien <viettienha98@gmail.com>
2020-08-20 11:12:12 +0700

    Project structure

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sun* Football Manager
+
+# How to run
+- Copy `src/main/resources/datasources_example.properties` to `src/main/resources/datasources.properties`
+- Config with your SQL connection
+- Config Tomcat server
---
+++ src/main/java/app/controller/HomeController.java
@@ -0,0 +1,28 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "views/home/index";
+    }
+
+//    @GetMapping("/user")
+//    public String testPermit(){
+//        return "views/home/index";
+//    }
+//
+//    @GetMapping("/login")
+//    public String login(){
+//        return "views/auth/login";
+//    }
+//
+//    @GetMapping("/register")
+//    public String register(){
+//        return "views/auth/register";
+//    }
+}
