013a93e9242bc87e2fce7ba2d1627c8ab3276216
Tien <viettienha98@gmail.com>
2020-09-04 16:25:44 +0700

    Add detail page

--- src/main/java/app/controller/FieldController.java
+++ src/main/java/app/controller/FieldController.java
@@ -1,14 +1,17 @@
 package app.controller;
 
+import app.model.Field;
 import app.service.FieldService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-public class FieldController {
+public class FieldController extends BaseController {
     private static final Logger logger = Logger.getLogger(FieldController.class);
 
     private final FieldService fieldService;
@@ -20,7 +23,6 @@ public class FieldController {
 
     @GetMapping(path = "/fields")
     public ModelAndView index(@RequestParam(value = "search", required = false) String search) {
-        logger.info("Index");
 
         ModelAndView model = new ModelAndView("views/fields/index");
         String title = "Field List";
@@ -36,4 +38,18 @@ public class FieldController {
         return model;
     }
 
+    @GetMapping(path = "/fields/{id}")
+    public String show(@PathVariable("id") int id, Model model, final RedirectAttributes redirectAttributes) {
+        String title = "Field Details";
+
+        Field field = fieldService.findField(id);
+
+        if (field == null)
+            return handleRedirect(redirectAttributes, "error", "Field type not found.", "/fields");
+
+        model.addAttribute("title", title);
+        model.addAttribute("data", field);
+
+        return "views/fields/show";
+    }
 }
