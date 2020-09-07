27c419adf91f96cafbd38876922942082b17a54e
Tien <viettienha98@gmail.com>
2020-09-07 16:49:59 +0700

    Field Pagination

--- src/main/java/app/dao/FieldDAO.java
+++ src/main/java/app/dao/FieldDAO.java
@@ -1,6 +1,8 @@
 package app.dao;
 
 import app.model.Field;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -8,7 +10,11 @@ public interface FieldDAO extends BaseDAO<Integer, Field> {
 
     Field findByName(String name);
 
-    List<Field> searchFields(String key);
+//    List<Field> searchFields(String key);
 
     List<Field> loadFields();
+
+    Page<Field> searchFields(String key, Pageable pageable);
+
+    Page<Field> paginateField(Pageable pageable);
 }
