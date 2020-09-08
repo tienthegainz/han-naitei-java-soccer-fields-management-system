365fe833f07c6292d1841ff43a5f2d035a3e7bff
NGUYEN DICH LONG <longnguyen25111998@gmail.com>
2020-09-08 15:50:24 +0700

    feat: create field review

--- src/main/java/app/controller/ReviewController.java
+++ src/main/java/app/controller/ReviewController.java
@@ -1,11 +1,17 @@
 package app.controller;
 
+import app.info.ReviewInfo;
+import app.model.User;
 import app.service.FieldService;
 import app.service.ReviewService;
 import app.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.lang.reflect.InvocationTargetException;
 
 @Controller
 public class ReviewController extends BaseController {
@@ -22,5 +28,22 @@ public class ReviewController extends BaseController {
         this.reviewService = reviewService;
         this.fieldService = fieldService;
         this.userService = userService;
+    }
+
+    @PostMapping(path = "/reviews")
+    public String post(ReviewInfo reviewInfo, final RedirectAttributes redirectAttributes) throws InvocationTargetException, IllegalAccessException {
+        logger.info("POST");
+
+        User currentUser = userService.getCurrentUser();
+        if (currentUser == null)
+            return handleRedirect(redirectAttributes, "error", "Unauthorized.", "/fields/" + reviewInfo.getField().getId());
+
+        reviewInfo.setId(null);
+        reviewInfo.setUser(currentUser);
+
+        if (reviewService.createReview(reviewInfo))
+            return handleRedirect(redirectAttributes, "success", "Review created.", "/fields/" + reviewInfo.getField().getId());
+
+        return handleRedirect(redirectAttributes, "error", "Error creating review.", "/fields" + reviewInfo.getField().getId());
     }
 }
--- src/main/java/app/service/ReviewService.java
+++ src/main/java/app/service/ReviewService.java
@@ -3,11 +3,14 @@ package app.service;
 import app.info.FieldInfo;
 import app.info.ReviewInfo;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public interface ReviewService {
 
     ReviewInfo findReview(int id);
+
+    boolean createReview(ReviewInfo fieldType) throws InvocationTargetException, IllegalAccessException;
 
     List<ReviewInfo> loadReviews(FieldInfo fieldInfo);
 
