559d01df17d86f8fb44556e58605f2d30da712b4
Tien <viettienha98@gmail.com>
2020-09-08 10:11:11 +0700

    [HotFix] Homepage, Refactor layout security

--- src/main/java/app/controller/HomeController.java
+++ src/main/java/app/controller/HomeController.java
@@ -10,11 +10,6 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String index(){
-        return "views/home/index";
-    }
-
     @GetMapping("/login")
     public String login(){
         return "views/auth/login";
@@ -25,18 +20,4 @@ public class HomeController {
         return "views/auth/register";
     }
 
-    @GetMapping("/detail-layout")
-    public String detailLayout(Model model){
-        Map<String, String> info = new HashMap<String, String>();
-        String title = "Field";
-        String image = "https://cdn1.sportngin.com/attachments/photo/1040/6534/Soccer_Field_large.jpg";
-        info.put("Name", "Handio Field");
-        info.put("Area", "12");
-        info.put("Address", "Handico, Pham Hung");
-        info.put("Price", "$25");
-        model.addAttribute("title", title);
-        model.addAttribute("allInfo", info);
-        model.addAttribute("image", image);
-        return "views/layout/detail-layout";
-    }
 }
